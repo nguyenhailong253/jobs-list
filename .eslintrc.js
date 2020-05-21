361e45478bb7249432afbf322467560b939ec711
Long Nguyen <nguyenhailong253@gmail.com>
2020-05-21 20:59:09 +1000

    Set up linting config and editor config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    extends: ['react-app', 'airbnb'],
+    plugins: [
+        'jest',
+        'sort-imports-es6-autofix',
+    ],
+    rules: {
+        'react/jsx-filename-extension': [
+            1,
+            {
+                'extensions': [
+                '.js',
+                '.jsx'
+                ]
+            }
+        ],
+        'import/extensions': 'off',
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        'sort-imports-es6-autofix/sort-imports-es6': 2,
+        'import/order': [
+            'error',
+            {
+                groups: [['builtin', 'external', 'internal']],
+                'newlines-between': 'always',
+            }
+        ],
+    }
+};
