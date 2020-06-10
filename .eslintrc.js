cfa9c813cd7f7e6b6bdb696f98e40a98d4d4a1a0
Long Nguyen <nguyenhailong253@gmail.com>
2020-06-10 20:47:35 +1000

    Create todolist module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,35 @@
 module.exports = {
-    extends: ['react-app', 'airbnb'],
-    plugins: [
-        'jest',
-        'sort-imports-es6-autofix',
+  extends: ['react-app', 'airbnb'],
+  parser: 'babel-eslint',
+  plugins: [
+    'jest',
+    'sort-imports-es6-autofix',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+        1,
+        {
+            'extensions': [
+            '.js',
+            '.jsx'
+            ]
+        }
     ],
-    rules: {
-        'react/jsx-filename-extension': [
-            1,
-            {
-                'extensions': [
-                '.js',
-                '.jsx'
-                ]
-            }
-        ],
-        'import/extensions': 'off',
-        'react/prop-types': 'off',
-        'react/require-default-props': 'off',
-        'sort-imports-es6-autofix/sort-imports-es6': 2,
-        'import/order': [
-            'error',
-            {
-                groups: [['builtin', 'external', 'internal']],
-                'newlines-between': 'always',
-            }
-        ],
-    }
+    'import/extensions': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'sort-imports-es6-autofix/sort-imports-es6': 2,
+    'import/order': [
+        'error',
+        {
+            groups: [['builtin', 'external', 'internal']],
+            'newlines-between': 'always',
+        }
+    ],
+    'arrow-parens': 'off',
+    'no-return-assign': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off'
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import './index.css';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-import RootModule from './modules/root/RootModule';
-import store from './modules/root/store';
+import TodoModule from './modules/todos/TodoModule';
+import rootReducer from './modules/todos/reducers';
+// import store from './modules/root/store';
+// import RootModule from './modules/root/RootModule';
 
 const rootElement = document.getElementById('root');
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <Provider store={store}>
-    <RootModule />
+    {/* <RootModule /> */}
+    <TodoModule />
   </Provider>,
   rootElement,
 );
